5c50c77b7dd003c59d4d510a87370c2349d22afd
zinebfadili <zineb.fadili@insa-lyon.fr>
2021-09-28 10:59:28 +0200

    working backward

--- deep_learning_project/test.py
+++ deep_learning_project/test.py
@@ -11,30 +11,30 @@ if __name__ == '__main__':
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
     correct = 0
     total = 0
-    with torch.no_grad():
-        # train
-        for epoch in range(2):
-            running_loss = 0.0
-            for i, data in enumerate(train_loader, 0):
-                images, labels = data
-                # zero the parameter gradients
-                optimizer.zero_grad()
-                outputs = net(images)
-                # print(outputs)
-                # break
-                # indice de la valeur max (0 pas face, 1, c'est face)
-                _, predicted = torch.max(outputs.data, 1)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
+    # train
+    for epoch in range(4):
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            images, labels = data
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            outputs = net(images)
+            # print(outputs)
+            # break
+            # indice de la valeur max (0 pas face, 1, c'est face)
+            _, predicted = torch.max(outputs.data, 1)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-                # print statistics
-                running_loss += loss.item()
-                if i % 2000 == 1999:    # print every 2000 mini-batches
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss / 2000))
-                    running_loss = 0.0
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
         # test
+    with torch.no_grad():
         for data in test_loader:
             images, labels = data
             outputs = net(images)
