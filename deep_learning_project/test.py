22a6997bd03eabb8bcdf6674092ad338bcd1f805
Stefan Ristovski <72705218+thestefanristovski@users.noreply.github.com>
2021-10-05 08:25:38 +0200

    update nb prints

--- deep_learning_project/test.py
+++ deep_learning_project/test.py
@@ -29,9 +29,9 @@ if __name__ == '__main__':
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
+            if i % 200 == 199:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
+                      (epoch + 1, i + 1, running_loss / 200))
                 running_loss = 0.0
         # test
     with torch.no_grad():
